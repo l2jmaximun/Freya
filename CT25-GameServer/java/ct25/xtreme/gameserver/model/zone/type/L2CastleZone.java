5dc44b1a8048b9658a7e27b83eef2ea06268898a
Rener <Rener@Rener-PC>
2017-12-19 03:57:16 -0200

    Add FlagZone Primavel

--- CT25-GameServer/java/ct25/xtreme/gameserver/model/zone/type/L2CastleZone.java
+++ CT25-GameServer/java/ct25/xtreme/gameserver/model/zone/type/L2CastleZone.java
@@ -3,104 +3,97 @@
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package ct25.xtreme.gameserver.model.zone.type;
 
-import ct25.xtreme.gameserver.datatables.MapRegionTable;
-import ct25.xtreme.gameserver.instancemanager.CastleManager;
+import ct25.xtreme.gameserver.ThreadPoolManager;
 import ct25.xtreme.gameserver.model.actor.L2Character;
 import ct25.xtreme.gameserver.model.actor.instance.L2PcInstance;
-import ct25.xtreme.gameserver.model.entity.Castle;
 import ct25.xtreme.gameserver.model.zone.L2SpawnZone;
+import ct25.xtreme.util.Rnd;
+
 
 /**
- * A castle zone
+ * An Custom PvP Zone
  *
- * @author  durgus
+ * @author  NeverMore
  */
-public class L2CastleZone extends L2SpawnZone
+public class L2CustomPvP extends L2SpawnZone
 {
-	private int _castleId;
-	private Castle _castle = null;
-	
-	public L2CastleZone(int id)
-	{
-		super(id);
-	}
-	
-	@Override
-	public void setParameter(String name, String value)
-	{
-		if (name.equals("castleId"))
-		{
-			_castleId = Integer.parseInt(value);
-		}
-		else
-			super.setParameter(name, value);
-	}
-	
-	@Override
-	protected void onEnter(L2Character character)
-	{
-		if (getCastle() != null)
-		{
-			character.setInsideZone(L2Character.ZONE_CASTLE, true);
-		}
-	}
-	
-	@Override
-	protected void onExit(L2Character character)
-	{
-		if (getCastle() != null)
-		{
-			character.setInsideZone(L2Character.ZONE_CASTLE, false);
-		}
-	}
-	
-	@Override
-	public void onDieInside(L2Character character)
-	{
-	}
-	
-	@Override
-	public void onReviveInside(L2Character character)
-	{
-	}
-	
-	/**
-	 * Removes all foreigners from the castle
-	 * @param owningClanId
-	 */
-	public void banishForeigners(int owningClanId)
-	{
-		for (L2Character temp : _characterList.values())
-		{
-			if (!(temp instanceof L2PcInstance))
-				continue;
-			if (((L2PcInstance) temp).getClanId() == owningClanId)
-				continue;
-			
-			((L2PcInstance) temp).teleToLocation(MapRegionTable.TeleportWhereType.Town);
-		}
-	}
-	
-	public int getCastleId()
-	{
-		return _castleId;
-	}
-	
-	private final Castle getCastle()
-	{
-		if (_castle == null)
-			_castle = CastleManager.getInstance().getCastleById(_castleId);
-		return _castle;
-	}
+    //Ramdom Locations configs
+    private static int[] _x = {11551, 10999, 10401};
+    private static int[] _y = {-24264, -23576, -24030};
+    private static int[] _z = {-3644, -3651, -3660 };
+    
+    public L2CustomPvP(int id)
+    {
+        super(5555);
+    }
+    
+    @Override
+    protected void onEnter(L2Character character)
+    {
+        character.setInsideZone(L2Character.ZONE_PVP, true);
+        character.setInsideZone(L2Character.ZONE_NOSUMMONFRIEND, true);
+        
+           if (character instanceof L2PcInstance)
+           {
+               ((L2PcInstance) character).sendMessage("You enter a PvP Area");
+               ((L2PcInstance) character).setPvpFlag(1);               
+           }
+    }
+    
+    @Override
+    protected void onExit(L2Character character)
+    {
+        character.setInsideZone(L2Character.ZONE_PVP, false);
+        character.setInsideZone(L2Character.ZONE_NOSUMMONFRIEND, false);
+        
+        if (character instanceof L2PcInstance)
+        {
+            ((L2PcInstance) character).stopNoblesseBlessing(null);
+            ((L2PcInstance) character).setPvpFlag(0);
+            ((L2PcInstance) character).sendMessage("You exit from a PvP Area");
+        }
+    }
+    
+    static class BackToPvp implements Runnable
+    {
+        private L2Character _activeChar;
+
+        BackToPvp(L2Character character)
+        {
+            _activeChar = character;
+        }
+
+        @Override
+        public void run()
+        {
+            int r = Rnd.get(3);
+            _activeChar.teleToLocation(_x[r] , _y[r], _z[r]);
+        }
+    }
+    
+    @Override
+    public void onDieInside(L2Character character)
+    {
+           if (character instanceof L2PcInstance)
+           {
+           }
+    }
+    
+    @Override
+    public void onReviveInside(L2Character character)
+    {
+        ThreadPoolManager.getInstance().scheduleGeneral(new BackToPvp(character), 500);
+        ((L2PcInstance) character).isNoblesseBlessed();
+    }
 }
